27d4202bee9ca3d5dd0be2be520012773de3f1d5
Fer <sombondrolo@gmail.com>
2022-05-28 20:29:29 -0300

    canary test, user test violate constraint with matcher

---
+++ spec/canary_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+# describe "rspec is configured properly" do
+#   it "should pass" do
+#     expect(true).to eq(true)
+#   end
+#   # it "can fail" do
+#   #   expect(false).to eq(true)
+#   # end
+# end
+
+describe "Etc testing group" do
+  it "Should string literal etc equals etc" do
+    expect("etc").to eq("etc")
+  end
+end
